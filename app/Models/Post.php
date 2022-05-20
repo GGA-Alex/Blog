5d4f08373d9952af560edc118361c471abd2004b
Alejandro Gaytan <alejandro.gaytan5785@alumnos.udg.mx>
2022-05-19 20:54:21 -0500

    Creación de las relaciones

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Cateory::class);
+    }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function image() {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
