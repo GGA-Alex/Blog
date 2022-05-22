9283a8bf8ee4bd7ac1e902b9570e3546322e5e6e
Alejandro Gaytan <alejandro.gaytan5785@alumnos.udg.mx>
2022-05-22 08:28:38 -0500

    Modificación al modelo Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,4 +20,9 @@ class Post extends Model
     public function tags() {
         return $this->belongsToMany(Tag::class);
     }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
